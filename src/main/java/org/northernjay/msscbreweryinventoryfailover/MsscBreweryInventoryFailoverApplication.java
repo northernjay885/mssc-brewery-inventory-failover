dfd90b4b28808221d1a442f94a90adc6e8983d6e
luorenjie <lrj193927@gmail.com>
2021-01-30 12:36:34 -0500

    init project

---
+++ src/main/java/org/northernjay/msscbreweryinventoryfailover/MsscBreweryInventoryFailoverApplication.java
@@ -0,0 +1,13 @@
+package org.northernjay.msscbreweryinventoryfailover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBreweryInventoryFailoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBreweryInventoryFailoverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/northernjay/msscbreweryinventoryfailover/MsscBreweryInventoryFailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.northernjay.msscbreweryinventoryfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryInventoryFailoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
